e92d59c75f9cb356bdaaa46f5ce713eb41df7c2d
jefersonla <oi_je@hotmail.com>
2021-11-19 21:49:29 -0300

    Ajustado build do projeto windows
    Former-commit-id: 90559278ed06b74648c0c7025bea5ee5de0f0991

---
+++ scripts/adjust-imports.js
@@ -0,0 +1,36 @@
+import { existsSync, writeFileSync, readFileSync } from 'fs';
+import { join, resolve, dirname }from 'path';
+import { fileURLToPath } from 'url';
+
+// Pega pasta atual
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/**
+ * Obtem a pasta raiz do projeto node.js
+ * @param pathDir
+ * @return {string|*}
+ */
+export const getRootDir = (pathDir = __dirname) => {
+    if (existsSync(join(pathDir, "package.json"))) {
+        return resolve(pathDir);
+    }
+
+    if (resolve(pathDir) === resolve(join(pathDir, ".."))) {
+        throw new Error(
+            "Falha em encontrar a rootDir do projeto. Verifique a existência de um package.json"
+        );
+    }
+
+    return getRootDir(join(pathDir, ".."));
+};
+
+// Define a pasta do arquivo
+const arqPath = resolve(getRootDir(__dirname), 'docs', 'index.html');
+
+// Pega e modifica conteudo
+const arqContent = readFileSync(arqPath, 'utf-8')
+    .replace(/="\//gm, '="');
+
+// Grava o arquivo
+writeFileSync(arqPath, arqContent);
+console.log('Reparando arquivo index.html');
